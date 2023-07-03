53297b5d3b561df5847d7239d56ced374246de3c
Eduardo Trujillo <eduardo.work.world@gmail.com>
2023-07-03 22:09:31 +0100

    -m define pipe and base class

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Bird:
     ROT_VEL = 20
     ANIMATION_TIME = 5
     
-    def __init__(self, x, y):
+    def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
         self.tilt = 0
@@ -109,9 +109,94 @@ class Bird:
         """
         return pygame.mask.from_surface(self.img)
     
+class Pipe:
+    GAP = 200
+    VEL = 5
     
+    def __init__(self, x) -> None:
+        self.x = x
+        self.height = 0
+        self.gap = 100
+        
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+        
+        self.passed = False
+        self.set_height()
+        
+    def set_height(self):
+        """
+        Define the height position where to draw the pipe between a range.
+        Adapt it to each pipe type (TOP or BOTTOM)
+        """
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+        
+    def move(self):
+        """ Select the speed move for the pipes in the screen """
+        self.x -= self.VEL
+        
+    def draw(self, win):
+        """ Print the elements in the screen """
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+        
+    def collide(self, bird, win):
+        """Determinate when collide between pipes and bird happens.
+
+        Args:
+            bird (_type_): _description_
+            win (_type_): _description_
+        """
+        
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+        
+        # To find the collide point.
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(bottom_mask, top_offset)
+        
+        if t_point or b_point:
+            return True     #collide happens.
     
-def draw_window(win, bird):
+        return False
+        
+        
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+    
+    def __init__(self, y) -> None:
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+        
+    def move(self):
+        """ Print 2 images and move one after another to generate infinite aspect.
+        """
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+            
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+            
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+    
+def draw_window(win, bird, pipes, base):
     """
         Draw the window with the elements.
 
@@ -120,13 +205,22 @@ def draw_window(win, bird):
         bird (_type_): _description_
     """
     win.blit(BG_IMG, (0,0))
+    
+    for pipe in pipes:
+        pipe.draw(win)
+        
+    base.draw(win)
+    
     bird.draw(win)
     pygame.display.update()
     
     
 def main():
-    bird = Bird(200,200)
+    bird = Bird(230,350)
+    base = Base(730)
+    pipes = [Pipe(700)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    
     clock = pygame.time.Clock()
     
     
@@ -139,7 +233,7 @@ def main():
                 run = False
                 
         bird.move()
-        draw_window(win, bird)
+        draw_window(win, bird, pipes, base)
         
     pygame.quit()
     quit()
